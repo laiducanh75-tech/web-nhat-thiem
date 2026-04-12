8360eb102d3e48c7eb7fae01beb4c6a9a2da111f
laiducanh75-tech <laiducanh75@gmail.com>
2026-04-12 11:49:57 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,13 @@
+import type { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "NHẤT THIÊN SƠN SÀI GÒN - LOGISTICS",
+  description: "Dịch vụ vận tải đường biển, đường bộ, hàng không uy tín tại TP.HCM",
+  icons: {
+    icon: "https://i.postimg.cc/bJtpQcDy/nts.jpg", 
+  },
+};
 
 export default function RootLayout({
   children,
