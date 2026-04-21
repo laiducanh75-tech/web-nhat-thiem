94dee70ec9a47eea154bbe5674e829c639c7e6cb
laiducanh75-tech <laiducanh75@gmail.com>
2026-04-21 21:52:56 +0700

    Update footer.tsx

--- components/logistics/footer.tsx
+++ components/logistics/footer.tsx
@@ -1,1 +1,8 @@
-
+<a 
+  href="https://www.facebook.com/nhatthiensonsaigon.vn" 
+  target="_blank" 
+  rel="noopener noreferrer"
+  className="flex items-center gap-2 hover:text-blue-500"
+>
+  <span>Theo dõi Nhật Thiêm trên Facebook</span>
+</a>
